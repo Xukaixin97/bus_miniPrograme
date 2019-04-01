97abdb2c983e9a18df5e7c3a57626d9d28e3820a
Xukaixin <xmd19970616@gmail.com>
2019-04-01 22:48:17 +0800

    添加iview
    添加iview 增加验证

--- pages/regist/regist.js
+++ pages/regist/regist.js
@@ -1,28 +1,23 @@
 // pages/regist/regist.js
 
 import WxValidate from '../../utils/WxValidate.js'
+const { $Toast } = require('../../dist/base/index');
 const app = getApp();
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-    //输入的验证码
-    code: '',
-    telephone:'',
-    //服务器返回的验证码
-    res_code: '',
-    isAgree: true
-  },
 
-  telephone: function(e) {
+  data: {
+    code: '',//输入的验证码
+    telephone: '',//手机号
+    res_code: '',//服务器返回的验证码
+    isAgree: true //统一协议
+  },
+  //赋值
+  telephone: function (e) {
     this.setData({
       telephone: e.detail.value,
     })
   },
-
-  code: function(e) {
+  code: function (e) {
     this.setData({
       code: e.detail.value
     })
@@ -35,32 +30,23 @@ Page({
     this.initValidate();
   },
 
-  showModal(error) {
-    wx.showModal({
-      content: error.msg,
-      showCancel: false,
-    })
-  },
-
   /**
    * 获取注册短信
    * @param {*} e 
    */
   getCode: function () {
-    console.log(this.data.code+"    "+this.data.telephone)
-    var that=this
-
+    var that = this
     wx.request({
-      url:"http://localhost:8080/user/getSMS",
-      method:"POST",
+      url: "http://localhost:8089/user/getSMS",
+      method: "POST",
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
       data: {
         'telephone': that.data.telephone
       },
-      success:function(res){
-        console.log(res.data)
+      success: function (res) {
+        that.data.res_code = res.data
       }
     })
   },
@@ -69,109 +55,71 @@ Page({
     const rules = {
       login_name: {
         required: true,
-        rangelength: [2,6]
+        rangelength: [6, 16]
       },
       login_password: {
         required: true,
-        login_password: true
+        checkPassword: true
       },
       telephone: {
         required: true,
         tel: true
-      }
+      },
+
     }
     const messages = {
       login_name: {
-        required: "输入用户名",
-        rangelength: "输入2-4个汉子"
+        required: "请输入用户名",
+        rangelength: "用户名长度为6-16个字符"
       },
       login_password: {
-        required: "输入密码",
-        rangelength: "输入2-4个汉子"
+        required: "请输入密码",
       },
       telephone: {
-        required: "输入11手机号",
+        required: "请输入手机号",
         tel: "输入正确的手机号码"
       }
+
     }
     this.WxValidate = new WxValidate(rules, messages);
+    this.WxValidate.addMethod('assistance', (value, param) => {
+      return this.WxValidate.optional(value) || (/^[a-zA-Z0-9_]{6,18}$/.test(value))
+  }, '密码由6-18位的英文大小、数字、下划线组成')
+  
   },
 
-  formSubmit(e) {
-    /**
-      * 4-3(表单提交校验)
-      */
-    const params = e.detail.value
-    
-    if (!this.WxValidate.checkForm(params)) {
-      const error = this.WxValidate.errorList[0]
-      this.showModal(error)
-      return false
-    }
-    /**
-    * 这里添写验证成功以后的逻辑
-    * 
-      */
-     wx.request({
-      url: 'http://localhost:8080/user/register',
-      method: 'POST',
-      header: { 'content-type': 'application/json'},
-      data: JSON.stringify(params),
-      success: function (res) {
-        console.log("回调函数:" + res.data)
-        var resData = res.data;
-        if (resData == true) {
-          wx.showToast({
-            title: '登录成功',
-            duration: 2000
-          })
-        } else {
-          wx.showToast({
-            title: '登录失败',
-            duration: 2000
-          })
-        }
+formSubmit(e) {
+  //4-3(表单提交校验)
+  const params = e.detail.value
+  if (!this.WxValidate.checkForm(params)) {
+    const error = this.WxValidate.errorList[0]
+    console.log(error)
+    $Toast({
+      content: error.msg,
+      type: 'error'
+    });
+    return false
+  }
+  wx.request({
+    url: 'http://localhost:8089/user/register',
+    method: 'POST',
+    header: { 'content-type': 'application/json' },
+    data: JSON.stringify(params),
+    success: function (res) {
+      console.log("回调函数:" + res.data)
+      var resData = res.data;
+      if (resData == true) {
+        $Toast({
+          content: '登陆成功',
+          type: 'success'
+        });
+      } else {
+        $Toast({
+          content: '登陆失败 ',
+          type: 'success'
+        });
       }
-    })
-
-    //验证通过以后->
-    this.submitInfo(params);
-  },
-  submitInfo(params) {
-    // form提交
-    let form = params;
-    console.log('将要提交的表单信息：', form);
-    wx.showToast({
-      title: '提交成功！！！！',
-    })
-  },
-  // // 注册函数
-  // formSubmit:function(e){
-  //   console.log(e.detail.value)
-  //   var that = this;
-  //   var formData = e.detail.value;
-    // wx.request({
-    //   url: 'http://localhost:8080/user/register',
-    //   method: 'POST',
-    //   header: { 'content-type': 'application/json'},
-    //   data: JSON.stringify(formData),
-    //   success: function (res) {
-    //     console.log("回调函数:" + res.data)
-    //     var resData = res.data;
-    //     if (resData == true) {
-    //       wx.showToast({
-    //         title: '登录成功',
-    //         duration: 2000
-    //       })
-    //     } else {
-    //       wx.showToast({
-    //         title: '登录失败',
-    //         duration: 2000
-    //       })
-    //     }
-    //   }
-    // })
-  // },
-
-
+    }
+  })
+},
 })
